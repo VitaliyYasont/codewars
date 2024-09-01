b8cf455b79e3f3b76682ed702bce878327a8d679
VitaliyYasont <zaider2016@yandex.ru>
2024-09-01 21:41:09 +0300

    2d izi commit

---
+++ ky8/convert-boolean-values-to-strings-'yes'-or-'no'.js
@@ -0,0 +1,11 @@
+// Завершите метод, который принимает логическое значение и возвращает "Yes" строку для true или "No" строку для false.
+
+
+// SOLUTION :
+
+function boolToWord( bool ){
+    return bool ? "Yes" : "No"; 
+  }
+
+  console.log(boolToWord(true));
+  
