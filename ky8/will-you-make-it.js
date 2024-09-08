0b618dc83c45cdd525b8586f20b237df5bca495f
VitaliyYasont <zaider2016@yandex.ru>
2024-09-08 16:51:19 +0300

    minimal

--- ky8/will-you-make-it.js
+++ ky8/will-you-make-it.js
@@ -14,3 +14,9 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     return mpg * fuelLeft >= distanceToPump
   }
+// Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
+
+function findLargest(n1, n2, n3) {
+    const max = Math.max(n1, n2, n3);
+    return max
+}
