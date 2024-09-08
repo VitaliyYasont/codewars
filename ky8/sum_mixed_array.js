369565b39b1078509ca5231fe5dfd256242883f2
VitaliyYasont <zaider2016@yandex.ru>
2024-09-08 16:04:11 +0300

    sum

---
+++ ky8/sum_mixed_array.js
@@ -0,0 +1,28 @@
+// ❓ Учитывая массив целых чисел в виде строк и чисел, верните сумму значений массива, 
+// как если бы все они были числами.
+// Верните свой ответ в виде числа.
+
+// ✅ SOLUTION:
+
+function sumMix(x){
+    let sum = 0;
+    for (let i = 0; i < x.length; i++){
+        let num = x[i];
+        let nums = Number(num);
+        if (isNaN(nums)) {
+            sum = sum + nums;
+        }
+    }
+    return sum;
+}
+
+  console.log(function sumMix('1', 4, 5, 6));
+  
+//   function sumMix(x) {
+//     let sum = 0;
+//     for (let i = 0; i < x.length; i++) {
+//       let a = x[i];
+//       sum = sum + (typeof a === 'string' ? Number(a) : a);
+//     }
+//     return sum;
+//   }
